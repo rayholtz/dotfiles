5da46f4932ea47383de150b326c62b09ae6b8c42
Ray Holtz <rayholtz@users.noreply.github.com>
2020-01-23 12:36:57 -0600

    tightened up config

--- Powershell5.1/Microsoft.PowerShell_profile.ps1
+++ Powershell5.1/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 function change-title {
-param ( [string] $newtitle)
-$host.ui.RawUI.WindowTitle = $newtitle + " - " + $host.ui.RawUI.WindowTitle;
+    param ( [string] $newtitle)
+    $host.ui.RawUI.WindowTitle = $newtitle + " - " + $host.ui.RawUI.WindowTitle;
 }
 
 Set-Location "C:\Users\Ray.Holtz\Documents\Powershell"
@@ -9,8 +9,8 @@ Set-Location "C:\Users\Ray.Holtz\Documents\Powershell"
 #set-alias cdvs "O:\My Documents\Powershell\Scripts\cdvs.ps1"
 #set-alias cdne "O:\My Documents\Powershell\Scripts\cdne.ps1"
 #set-alias cdzz "O:\My Documents\Powershell\Scripts\cdzz.ps1"
-set-alias remcon "Open-VMConsoleWindow"
 #set-alias tracert "E:\Downloads\Traceroute-WinMTR-v092\WinMTR.exe"
+set-alias remcon "Open-VMConsoleWindow"
 set-alias convs "$home\Documents\WindowsPowershell\Scripts\convs.ps1"
 set-alias opendomain "$home\Documents\WindowsPowershell\Scripts\opendomain.ps1"
 
@@ -25,22 +25,3 @@ function prompt {
     Write-host $curPath -foregroundcolor cyan
     "$('>' * ($nestedPromptLevel + 1)) "
 }
-
-#$env:PSModulePath.split(';')
-
-<#
-$env:PSModulePath = $env:PSModulePath -replace "\\rovsfile1\users\rholtz\My Documents\Powershell\Modules;"
-$env:PSModulePath = "O:\My Documents\Powershell\Modules\;" + $env:PSModulePath
-#>
-
-<#
-#Save the current value in the $p variable.
-$p = [Environment]::GetEnvironmentVariable("PSModulePath")
-
-#Add the new path to the $p variable. Begin with a semi-colon separator.
-$p += ";O:\My Documents\Powershell\Modules\"
-
-#Add the paths in $p to the PSModulePath value.
-[Environment]::SetEnvironmentVariable("PSModulePath",$p)
-#>
-
