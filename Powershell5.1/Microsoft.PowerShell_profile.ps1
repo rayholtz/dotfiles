fca8819538dd6aebaa7df74a6c003bad6afbc984
Ray Holtz <rayholtz@users.noreply.github.com>
2023-04-14 12:23:49 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Powershell5.1/Microsoft.PowerShell_profile.ps1
+++ Powershell5.1/Microsoft.PowerShell_profile.ps1
@@ -5,14 +5,11 @@ function change-title {
 
 Set-Location "C:\Users\Ray.Holtz\Documents\Powershell"
 
-#set-alias cdpo "O:\My Documents\Powershell\Scripts\cdpo.ps1"
-#set-alias cdvs "O:\My Documents\Powershell\Scripts\cdvs.ps1"
-#set-alias cdne "O:\My Documents\Powershell\Scripts\cdne.ps1"
-#set-alias cdzz "O:\My Documents\Powershell\Scripts\cdzz.ps1"
-#set-alias tracert "E:\Downloads\Traceroute-WinMTR-v092\WinMTR.exe"
 set-alias remcon "Open-VMConsoleWindow"
 set-alias convs "$home\Documents\WindowsPowershell\Scripts\convs.ps1"
+set-alias getcreds "$home\Documents\WindowsPowershell\Scripts\getcreds.ps1"
 set-alias opendomain "$home\Documents\WindowsPowershell\Scripts\opendomain.ps1"
+import-module "$home\Documents\WindowsPowershell\Scripts\Get-LastError.ps1"
 
 function prompt {
     $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
